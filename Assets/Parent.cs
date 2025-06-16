c6513cf66e9a5e50a453706eca409640c2d8e7a8
Niels69420 <593900@edu.rocmn.nl>
2025-06-16 09:28:08 +0200

    Reverting some changes

--- Assets/Parent.cs
+++ Assets/Parent.cs
@@ -4,16 +4,6 @@ public class Parent : MonoBehaviour
 {
     [SerializeField] string playerTag = "Player";
     [SerializeField] Transform platform;
-    [SerializeField] float platformMoveSpeed = 2f; // Speed for platform movement
-
-    private void Update()
-    {
-        if (platform != null)
-        {
-            // Move the platform forward in its local Z direction
-            platform.position += platform.right * platformMoveSpeed * Time.deltaTime;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
